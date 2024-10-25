6e66b7ec64ccc982991403784ea0ab7ba2baefbe
adminsisitery <1323880492@qq.com>
2024-10-25 10:12:57 +0800

    修改

--- game/src/com/fei/ui/tellsome.java
+++ game/src/com/fei/ui/tellsome.java
@@ -2,8 +2,10 @@ package com.fei.ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class tellsome extends JFrame {
+public class tellsome extends JFrame implements ActionListener {
     String iwentsay = "<html><b>注册没有做^-^<br>(懒 :( <br>十分感谢您体验本游戏</b></html>"; // 使用HTML标签支持换行
     String mm = "<html>用户名：123<br> 密码：123<br>直接拿去登录吧</html>";
     // 构造函数
@@ -26,10 +28,18 @@ public class tellsome extends JFrame {
 
     // 初始化视图
     public void initview() {
+
+
+
         // 添加背景图片
         JLabel background = new JLabel(new ImageIcon("image\\tks.png"));
         background.setBounds(0, 0, 600, 500); // 确保背景图覆盖整个窗口
         this.getContentPane().add(background);
+
+        JButton registerButton = new JButton("确定");
+        registerButton.setBounds(235, 400, 90, 40);
+        this.getContentPane().add(registerButton);
+        registerButton.addActionListener(this);
 
         // 添加文字标签
         JLabel textLabel = new JLabel(iwentsay);
@@ -46,10 +56,13 @@ public class tellsome extends JFrame {
         // 将文字标签添加到背景标签上
         background.add(textLabel2);
         background.add(textLabel);
+        background.add(registerButton);
     }
 
-    public static void main(String[] args) {
-        // 创建并显示窗口
-        new tellsome();
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.setVisible(false);
+        new  LoginJFrame();
     }
 }
